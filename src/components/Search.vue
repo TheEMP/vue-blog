ee2fd62f9a7368f94b8ef5b2a05fe3e707cb1392
justinandrewfien <jfien12@live.com>
2017-02-16 11:21:11 -0700

    Search adjustments

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,7 @@
             <div class="nav-wrapper">
                 <form>
                     <div class="input-field">
-                        <input id="search" type="search" required>
+                        <input id="search" type="search" v-model="searchString" required>
                         <label class="label-icon" for="search"><i class="material-icons">search</i></label>
                         <i class="material-icons">close</i>
                     </div>
@@ -18,7 +18,29 @@
 
 <script>
     export default {
-        name: 'search'
+        name: 'search',
+        data() {
+            return {
+                questions: [],
+                searchString: ''
+            }
+        },
+        computed: {
+            filteredQuestions: function () {
+
+                var questArray = this.questions
+                var userQuery = this.searchString
+                if (userQuery.length > 0) {
+                    userQuery = userQuery.trim().toLowerCase();
+                    questArray = questArray.filter(function (item) {
+                        if (item.title.toLowerCase().indexOf(userQuery) !== -1) {
+                            return item;
+                        }
+                    })
+                    return questArray;
+                }
+            }
+        }
     }
 
 </script>
