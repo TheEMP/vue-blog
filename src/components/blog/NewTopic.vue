b850005c7b81dfcfd34ee42fae494245e093429a
TheEMP <gunnarDebate@gmail.com>
2017-02-16 11:21:02 -0700

    Fix new topix

--- src/components/blog/NewTopic.vue
+++ src/components/blog/NewTopic.vue
@@ -3,37 +3,37 @@
         <div class="card-content white-text">
             <form @submit.prevent="post">
                 <div class="row">
-                    <div class="input-field">
+                    <div class="input-field col s12">
                         <input id="inputtile1" type="text" v-model="title" />
                         <label for="inputtile1">Blog Title</label>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <textarea id="textarea1" class="materialize-textarea" rows="8" cols="100" v-model="text"></textarea>
-                            <label for="textarea1">Textarea</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="iconLable" type="text" v-model="icon" />
-                            <label for="iconLable">Blog Icon</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s6 offset-s3">
-                            <div class="chips chips-placeholder"></div>
-                        </div>
-                    </div>
-
-
-                    <blockquote>
-                        <VueMarkdown class="left-align" :source="text">
-                        </VueMarkdown>
-                    </blockquote>
-                    <button class="waves-effect waves-red red btn" type="submit">Post!</button>
-                    <a id="iconshowcase" class="btn-floating disabled"><i class="material-icons">{{icon}}</i></a>
-                    <label for="iconshowcase">Blog Title</label>
                 </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <textarea id="textarea1" class="materialize-textarea" rows="8" cols="100" v-model="text"></textarea>
+                        <label for="textarea1">Textarea</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="iconLable" type="text" v-model="icon" />
+                        <label for="iconLable">Blog Icon</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s6 offset-s3">
+                        <div class="chips chips-placeholder"></div>
+                    </div>
+                </div>
+
+
+                <blockquote>
+                    <VueMarkdown class="left-align" :source="text">
+                    </VueMarkdown>
+                </blockquote>
+                <button class="waves-effect waves-red red btn" type="submit">Post!</button>
+                <a id="iconshowcase" class="btn-floating disabled"><i class="material-icons">{{icon}}</i></a>
+                <label for="iconshowcase">Blog Title</label>
             </form>
 
         </div>
@@ -42,8 +42,8 @@
 
 <script>
     import VueMarkdown from "vue-markdown"
-   // import axios from 'axios'
-   // import cookies from '../../assets/getCookies.js'
+    // import axios from 'axios'
+    // import cookies from '../../assets/getCookies.js'
     import Router from '../../router'
     export default {
         name: "newtopic",
@@ -53,10 +53,10 @@
                     name: this.title,
                     text: this.text,
                     userId: cookies("userId"),
-                    tags: this.tags, 
+                    tags: this.tags,
                     icon: this.icon
                 }).then(res => {
-                    Router.push({ name: 'thread-show', params: { id:res.data.data._id }})
+                    Router.push({ name: 'thread-show', params: { id: res.data.data._id } })
 
                     console.log(res)
                 }).catch(err => {
@@ -75,19 +75,38 @@
         components: {
             VueMarkdown
         },
-        mounted(){
+        activated() {
             $('.chips-placeholder').material_chip({
                 placeholder: 'Enter a tag',
                 secondaryPlaceholder: '+Tag',
             });
-            $('.chips').on('chip.add', (e, chip)=>{
+            $('.chips').on('chip.add', (e, chip) => {
                 this.tags.push(chip.tag)
             });
 
-            $('.chips').on('chip.delete', (e, chip)=>{
+            $('.chips').on('chip.delete', (e, chip) => {
                 for (var i = 0; this.tags.length; i++) {
                     var tag = this.tags[i]
-                    if (tag === chip.tag){
+                    if (tag === chip.tag) {
+                        this.tags.splice(i, 1)
+                    }
+                }
+            });
+
+        },
+        created() {
+            $('.chips-placeholder').material_chip({
+                placeholder: 'Enter a tag',
+                secondaryPlaceholder: '+Tag',
+            });
+            $('.chips').on('chip.add', (e, chip) => {
+                this.tags.push(chip.tag)
+            });
+
+            $('.chips').on('chip.delete', (e, chip) => {
+                for (var i = 0; this.tags.length; i++) {
+                    var tag = this.tags[i]
+                    if (tag === chip.tag) {
                         this.tags.splice(i, 1)
                     }
                 }
